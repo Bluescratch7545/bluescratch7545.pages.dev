4fdd7cf456fda4ca63f8eaee4b9755ca4d3bbe47
Bluescratch <dzadu02@gmail.com>
2025-12-31 13:45:29 +0100

    updet1

--- scripts.js
+++ scripts.js
@@ -6,3 +6,21 @@ document.addEventListener('DOMContentLoaded', () => {
         }, index * 200);
     });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const planes = document.querySelectorAll("img.fly-in");
+    planes.forEach((plane, index) => {
+        setTimeout(() => {
+            plane.classList.add('visible');
+        }, index * 300);
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const imgs = document.querySelectorAll('img.imgag');
+    imgs.forEach((img, index) => {
+        setTimeout(() => {
+            img.classList.add('visible')
+        }, index * 200);
+    });
+});
